db42b6a39d81bca01ea9d1258261ac3f360e7bc7
Y4nnLS <101208372+Y4nnLS@users.noreply.github.com>
2023-09-03 14:59:45 -0300

    adição do docstring no ex3.py

--- ex3.py
+++ ex3.py
@@ -18,6 +18,10 @@ letras_palavra = []
 
 
 def imprime_teclado(teclado, teclas_usadas, letras_lugar_certo, letras_certas):
+    """
+    Imprime o layout do teclado, formatado com letras coloridas, apresentando se a letra está certa e no lugar correto,
+    letra certa mas no lugar errado ou se a letra não está presente na palavra
+    """
     print('')
     for linha in teclado:
         teclado_formatado = []
@@ -40,32 +44,42 @@ def imprime_teclado(teclado, teclas_usadas, letras_lugar_certo, letras_certas):
 
 
 def remove_letras_usadas(palavra):
+    """
+    Adiciona as letras das palavras usadas para a lista de teclas usadas
+    """
     for letra in palavra:
         if letra not in teclas_usadas:
             teclas_usadas.append(letra)
 
 
 def escolhe_palavra():
+    """
+    Escolhe uma palavra aleatória do arquivo "lista_palavras.txt", baseado no critério especificado do tamanho da palavra
+    """
     arquivo = open(
-        "X:\VSCode\Yann\Python ListaExercícios1\words.txt", "r", encoding="UTF-8")
+        "X:\VSCode\Yann\Python ListaExercícios1\lista_palavras.txt", "r", encoding="UTF-8")
     palavras = []
     for linha in arquivo:
         linha = linha.strip().lower()
         palavras.append(linha)
     arquivo.close()
-    palavra_secreta = random.choice(palavras)
-    print(palavra_secreta)
+    palavra_secreta = numero_letras(palavras)
+
+    # print(palavra_secreta) para fins de testes, essa linha imprime a palavra_secreta para verificação do programador
     return palavra_secreta
 
 
 def numero_letras(palavras):
+    """
+    Filtra as palavras de acordo com o tamanho especificado pelo usuario.
+    Caso ele não encontre uma palavra com o tamanho especificado, ele escolhe uma palavra aleatoria de qualquer tamanho
+    """
     num_letras = int(input("Digite o número de letras da palavra secreta: "))
     palavra_x_letras = []
     [palavra_x_letras.append(x) for x in palavras if int(len(x)) == num_letras]
     if (len(palavra_x_letras) == 0):
         print("Não existe palavras com a quantidade de caracteres especificados, escolhendo uma palavra aleatória...")
-        numero_palavra = random.randrange(0, len(palavras))
-        palavra_secreta = palavras[numero_palavra].upper()
+        palavra_secreta = random.choice(palavras)
         return palavra_secreta
     else:
         print("Escolhendo uma palavra...")
@@ -74,64 +88,87 @@ def numero_letras(palavras):
 
 
 emojis = {
-    'correct_place': '🟩',
-    'correct_letter': '🟨',
-    'incorrect': '⬜'
+    'lugar_certo': '🟩',
+    'letra_certa': '🟨',
+    'errado': '⬜'
 }
 
 
-def correct_place(letter):
-    letras_lugar_certo.append(letter)
-    return f'[black on green]{letter}[/]'
+def lugar_certo(letra):
+    """
+    Armazena a letra em letras_lugar_certo
+    """
+    letras_lugar_certo.append(letra)
+    return f'[black on green]{letra}[/]'
 
 
-def correct_letter(letter):
-    letras_certas.append(letter)
-    return f'[black on yellow]{letter}[/]'
+def letra_certa(letra):
+    """
+    Armazena a letra em letras_certas
+    """
+    letras_certas.append(letra)
+    return f'[black on yellow]{letra}[/]'
 
 
-def incorrect(letter):
-    teclas_usadas.append(letter)
-    return f'[black on white]{letter}[/]'
+def errado(letra):
+    """
+    Armazena a letra em teclas_usadas
+    """
+    teclas_usadas.append(letra)
+    return f'[black on white]{letra}[/]'
 
 
-def score_guess(guess, answer):
-    scored = []
-    emojied = []
-    for i, letter in enumerate(guess):
-        if answer[i] == guess[i]:
-            scored += correct_place(letter)
-            emojied.append(emojis['correct_place'])
-        elif letter in answer:
-            scored += correct_letter(letter)
-            emojied.append(emojis['correct_letter'])
+def pontos_chute(chute, palavra):
+    """
+    Calcula e formata os pontos e os emojies baseado no chute do jogador
+    """
+    pontos = []
+    emoji = []
+    for i, letra in enumerate(chute):
+        if palavra[i] == chute[i]:
+            pontos += lugar_certo(letra)
+            emoji.append(emojis['lugar_certo'])
+        elif letra in palavra:
+            pontos += letra_certa(letra)
+            emoji.append(emojis['letra_certa'])
         else:
-            scored += incorrect(letter)
-            emojied.append(emojis['incorrect'])
-    return ''.join(scored), ''.join(emojied)
+            pontos += errado(letra)
+            emoji.append(emojis['errado'])
+    return ''.join(pontos), ''.join(emoji)
 
 
 def recebe_chute():
+    """
+    Recebe e processa o chute do jogador
+    """
     chute = input("Digite uma palavra: ")
     chute = chute.strip().lower()
     return chute
 
 
 def jogar():
+    """
+    Função principal do jogo para controlar o fluxo do jogo
+    """
     console = Console()
     tentativas = 0
     palavra_secreta = escolhe_palavra()
-    all_emojied = []
-    all_scored = []
+    emojies = []
+    todos_pontos = []
     while tentativas <= MAX_TENTATIVAS:
         tentativas += 1
         chute = recebe_chute()
+        if len(chute) != len(palavra_secreta):
+            print(
+                f"Apenas palavras com {len(palavra_secreta)} letras são válidas")
+            tentativas -= 1
+            continue
         remove_letras_usadas(chute)
-        scored, emojied = score_guess(chute, palavra_secreta)
-        all_scored.append(scored)
-        all_emojied.append(emojied)
-        for scored in all_scored:
-            console.print(scored)
+        pontos, emoji = pontos_chute(chute, palavra_secreta)
+        todos_pontos.append(pontos)
+        emojies.append(emoji)
+        for pontos in todos_pontos:
+            console.print(pontos)
 
         imprime_teclado(teclado, teclas_usadas,
                         letras_lugar_certo, letras_certas)
@@ -139,7 +176,7 @@ def jogar():
         if chute == palavra_secreta:
             print("Parabéns!! Você ganhou")
             break
-    for em in all_emojied:
+    for em in emojies:
         print(em)
     print(f"A palavra secreta era: {palavra_secreta}")
 
